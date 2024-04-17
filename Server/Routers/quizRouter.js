04f2f926a3fc37972a2361d17371ff1698f010aa
annanay aggarwal <anni.agg2003@gmail.com>
2024-04-17 12:08:02 +0530

    port env

--- Server/Routers/quizRouter.js
+++ Server/Routers/quizRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const router = express.Router();
 const { quiz, submit } = require("../Controllers/quizController");
 
--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
 const quizrouter = require("./Routers/quizRouter");
 
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 app.use("/v1/api", quizrouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
